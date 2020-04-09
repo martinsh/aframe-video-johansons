1db15e53035243eb195fc1b788c60b55d49eb05d
martinsh <artmartinsh@gmail.com>
2020-04-09 09:49:54 +0300

    player

--- js/AVideoPlayer.js
+++ js/AVideoPlayer.js
@@ -4,7 +4,7 @@ var AVideoPlayer = function() {
   this.duration         = 0;
   this.current_progress = 0;
   this.progressWidth    = 4;
-  this.paused           = false;
+  this.paused           = true;
 
   // Elems
   this.elProgressBar   = null;
